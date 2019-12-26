89cc3dd175542a15518d06d9470e3a3311045570
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-26 16:23:09 -0500

    Use static System.Console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace date_classes
 {
@@ -6,7 +7,7 @@ namespace date_classes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
         }
     }
 }
